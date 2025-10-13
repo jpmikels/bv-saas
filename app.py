b880db1e178fafc4af621d73252799dd6845d54c
jpmikels <jasen.mikels@ccbank.com>
2025-10-13 13:46:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -44,6 +44,6 @@ def extract_pdf_data(path):
     return extracted_data
 
 if __name__ == '__main__':
-    # This asks Google for the secret channel number!
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
