441084fd658f473215f0a0d089398ed9eb919111
jpmikels <jasen.mikels@ccbank.com>
2025-10-13 12:30:43 -0600

    Update app.py

--- bv-saas-starter/app.py
+++ bv-saas-starter/app.py
@@ -44,4 +44,6 @@ def extract_pdf_data(path):
     return extracted_data
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    # This asks Google for the secret channel number!
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
